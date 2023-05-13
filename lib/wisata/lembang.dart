3c6acd9ca0e15edfee88201a025f42bec8638b43
devinaats <133020963+devinaats@users.noreply.github.com>
2023-05-13 10:42:01 +0700

    lembang.dart

--- lib/wisata/lembang.dart
+++ lib/wisata/lembang.dart
@@ -93,7 +93,7 @@ Widget QuoteCard() => Card(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Ahmad Zakaria',
+          'Athalla',
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
@@ -114,7 +114,7 @@ Widget QuoteCard1() => Card(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Muhammad Aditya Nursyahid',
+          'Devina',
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
